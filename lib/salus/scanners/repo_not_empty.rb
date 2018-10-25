c3990962017cb094a270190fd2fd0a7c7b84ab1c
Adam Richardson <adam.richardson@coinbase.com>
2018-10-25 13:30:55 -0400

    [chore] clean up and fix specs for repo_not_empty.rb

--- lib/salus/scanners/repo_not_empty.rb
+++ lib/salus/scanners/repo_not_empty.rb
@@ -9,12 +9,11 @@ module Salus::Scanners
     def run
       # We check there is at least one item in this repo.
       if directory_empty?
-        report_failure
-        report_info(
-          'problem',
+        report_error(
           'Salus was run on a blank directory. This may indicate misconfiguration '\
           'such as not correctly voluming in the repository to be scanned.'
         )
+        report_failure
       else
         report_success
       end
