86a2a66bbe5e15c3c65347eb8906e52603e96c58
Adam Richardson <adam.richardson@coinbase.com>
2018-10-25 13:32:09 -0400

    [chore] clean up and fix specs for report_python_modules.r

--- lib/salus/scanners/report_python_modules.rb
+++ lib/salus/scanners/report_python_modules.rb
@@ -4,27 +4,23 @@ require 'salus/scanners/base'
 
 module Salus::Scanners
   class ReportPythonModules < Base
-    DEP_PARSER = File.read(File.join(__dir__, 'report_python_modules.py'))
-
     def run
-      Dir.chdir(@repository.path_to_repo) do
-        shell_return = run_shell('python', stdin_data: DEP_PARSER)
+      shell_return = run_shell(['bin/report_python_modules', @repository.path_to_repo])
 
-        if shell_return.success?
-          dependencies = JSON.parse(shell_return.stdout)
-          dependencies.each do |name, version|
-            record_dependency_info(
-              {
-                type: 'python_requirement',
-                name: name,
-                version: version
-              },
-              'requirements.txt'
-            )
-          end
-        else
-          report_error('message' => shell_return.stderr)
-        end
+      if !shell_return.success?
+        report_error(shell_return.stderr)
+        return
+      end
+
+      dependencies = JSON.parse(shell_return.stdout)
+
+      dependencies.each do |name, version|
+        report_dependency(
+          'requirements.txt',
+          type: 'python_requirement',
+          name: name,
+          version: version
+        )
       end
     end
 
--- spec/lib/salus/scanners/report_python_modules_spec.rb
+++ spec/lib/salus/scanners/report_python_modules_spec.rb
@@ -1,43 +1,49 @@
 require_relative '../../../spec_helper.rb'
 
 describe Salus::Scanners::ReportPythonModules do
-  let(:blank_config) { {} }
-  let(:report) { Salus::Report.new }
-  let(:scan_report) { json_report['scans']['ReportPythonModules'] }
-  let(:dependencies) { scan_report['info']['dependency'] }
-
   describe '#should_run?' do
-    context 'no requirements file present' do
-      it 'should return false' do
-        repo = Salus::Repo.new('spec/fixtures/blank_repository')
-        scanner = Salus::Scanners::ReportPythonModules.new(
-          repository: repo,
-          report: report,
-          config: blank_config
-        )
-        expect(scanner.should_run?).to eq(false)
-      end
+    it 'should return false in the absence of requirements.txt' do
+      repo = Salus::Repo.new('spec/fixtures/blank_repository')
+      scanner = Salus::Scanners::ReportPythonModules.new(repository: repo, config: {})
+      expect(scanner.should_run?).to eq(false)
+    end
+
+    it 'should return true if requirements.txt is present' do
+      repo = Salus::Repo.new('spec/fixtures/python/requirements_unpinned')
+      scanner = Salus::Scanners::ReportPythonModules.new(repository: repo, config: {})
+      expect(scanner.should_run?).to eq(true)
     end
   end
-  describe '#run' do
-    context 'basic requirements.txt' do
-      it 'should report requirements' do
-        repo = Salus::Repo.new('spec/fixtures/python/requirements_unpinned')
-        scanner = Salus::Scanners::ReportPythonModules.new(
-          repository: repo,
-          report: report,
-          config: blank_config
-        )
-        expect(scanner.should_run?).to eq(true)
 
-        scanner.run
-        expect(scan_report).not_to eq(nil), "ERROR: #{JSON.pretty_generate(json_report)}"
+  it 'should report modules from requirements.txt' do
+    repo = Salus::Repo.new('spec/fixtures/python/requirements_unpinned')
+    scanner = Salus::Scanners::ReportPythonModules.new(repository: repo, config: {})
 
-        deps = dependencies.map { |dep| [dep['name'], dep['version']] }
-        expect(deps).to include(%w[requests >=2.5])
-        expect(deps).to include(%w[six >=1.9])
-        expect(deps).to include(%w[pycryptodome >=3.4.11])
-      end
-    end
+    scanner.run
+
+    dependencies = scanner.report.to_h.fetch(:info).fetch(:dependencies)
+
+    expect(dependencies).to match_array(
+      [
+        {
+          dependency_file: 'requirements.txt',
+          name: 'requests',
+          version: '>=2.5',
+          type: 'python_requirement'
+        },
+        {
+          dependency_file: 'requirements.txt',
+          name: 'six',
+          version: '>=1.9',
+          type: 'python_requirement'
+        },
+        {
+          dependency_file: 'requirements.txt',
+          name: 'pycryptodome',
+          version: '>=3.4.11',
+          type: 'python_requirement'
+        }
+      ]
+    )
   end
 end
