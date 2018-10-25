de6b93a1c7726cd9660ddc5f6c0226bc4792e594
Adam Richardson <adam.richardson@coinbase.com>
2018-10-25 13:30:55 -0400

    [chore] clean up and fix specs for npm_audit.rb

--- lib/salus/scanners/npm_audit.rb
+++ lib/salus/scanners/npm_audit.rb
@@ -8,13 +8,13 @@ module Salus::Scanners
     ADVISORY_URL_REGEX = %r{^https://nodesecurity.io/advisories/(\d+)$}
 
     def run
-      exceptions = @config['exceptions']
-
+      exceptions = @config.fetch('exceptions', [])
       created_package_lock = false
+
       if !@repository.package_lock_json_present?
         Dir.chdir(@repository.path_to_repo) do
           report_info(
-            'package_lock_missing',
+            :package_lock_missing,
             'No package.lock file was found, so we generated one for you. As long as '\
               'you use yarn, this should be fine. If not, please check in a package-lock.json '\
               'file into your source control.'
@@ -24,45 +24,41 @@ module Salus::Scanners
         end
       end
 
-      Dir.chdir(@repository.path_to_repo) do
-        shell_return = run_shell('npm audit --json')
+      shell_return = nil
+      Dir.chdir(@repository.path_to_repo) { shell_return = run_shell('npm audit --json') }
 
-        if shell_return.success?
-          report_success
-        else
-          # Parse output
-          npm_audit_report = JSON.parse(shell_return.stdout)
-          report_stdout(shell_return.stdout)
+      return report_success if shell_return.success?
 
-          # Report scan output
-          report_info('npm_audit_output', npm_audit_report)
+      # Parse output
+      npm_audit_report = JSON.parse(shell_return.stdout, symbolize_names: true)
 
-          # Report ignored advisories after validating the keys all exist
-          exceptions&.each do |exception|
-            if exception.keys.sort != %w[advisory_id changed_by notes]
-              report_error("The exception #{exception} doesn't have a proper format! Please ensure"\
-              ' that each exception has an `advisory_id`, `changed_by`, and `notes` field!')
-            end
-            report_info('exceptions', exception)
-          end
+      # Report scan output
+      report_info(:npm_audit_output, npm_audit_report)
 
-          exception_ids = exceptions&.map { |x| x['advisory_id'] } || []
-          active_vuln_ids = npm_audit_report['advisories'].keys - exception_ids
-
-          active_vuln_ids.empty? ? report_success : report_failure
-        end
-
-        # Cleanup, mostly for local dev since we run in Docker normally
-        File.delete('package-lock.json') if created_package_lock
+      # FIXME(as3richa): emit some sort of warning or logline
+      # for exceptions that don't conform to the spec
+      exceptions = exceptions.select do |exception|
+        exception.is_a?(Hash) &&
+          exception.keys.sort == %w[advisory_id changed_by notes]
       end
+
+      report_info(:exceptions, exceptions)
+
+      exception_ids = exceptions.map { |exception| exception['advisory_id'] }
+      active_vuln_ids = npm_audit_report[:advisories].keys.map(&:to_s) - exception_ids
+
+      report_info(:vulnerabilities, active_vuln_ids)
+
+      active_vuln_ids.empty? ? report_success : report_failure
+    ensure
+      # Cleanup, mostly for local dev since we run in Docker normally
+      File.delete("#{@repository.path_to_repo}/package-lock.json") if created_package_lock
     end
 
     def should_run?
-      [
-        @repository.package_json_present?,
-        @repository.package_lock_json_present?,
+      @repository.package_json_present? ||
+        @repository.package_lock_json_present? ||
         @repository.yarn_lock_present?
-      ].any?
     end
   end
 end
