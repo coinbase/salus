c80115c62e94398f609e7ee422f20af4af19442c
Adam Richardson <as3richa@uwaterloo.ca>
2019-01-17 20:08:41 -0500

    properly propagate brakeman errors to the user

---
+++ spec/fixtures/brakeman/rails_app_with_syntax_error/app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+:;sda;'adsf;'ladskf;sA:Ds;'flk;dsjrp98 327r8qtgfsvo; lcx,x/vcZ';o&&&&'&&!
+end
