4fe0973a903fe3e6cbada30b26661bb8d7426048
Nishil Shah <nishil.shah@coinbase.com>
2019-10-03 18:13:01 -0700

    golang 1.13.1 has some build issues that do not exist in 1.12.10 and running go get can fix some gosec build issues

--- lib/salus/scanners/gosec.rb
+++ lib/salus/scanners/gosec.rb
@@ -12,6 +12,9 @@ module Salus::Scanners
       #   - -fmt=json for JSON output
       #   - gosec can scan go modules as of 2.0.0.
       shell_return = Dir.chdir(@repository.path_to_repo) do
+        # sometimes the go.sum needs to be forced updated to be able to correctly build packages.
+        # focing go get seems to do the trick
+        run_shell("go get")
         cmd = "gosec #{config_options}-fmt=json ./..."
         run_shell(cmd)
       end
