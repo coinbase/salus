d68ca1df4f4d63038734e4e10294f53c82444662
cbtoni <64813948+cbtoni@users.noreply.github.com>
2020-05-12 15:11:03 -0700

    move Bugsnag configuration

--- lib/salus/bugsnag.rb
+++ lib/salus/bugsnag.rb
@@ -1,3 +1,19 @@
+require 'bugsnag'
+require 'English'
+
+# If present, configure Bugsnag globally to capture all errors
+# including both handled and unhandled execptions
+if ENV['BUGSNAG_API_KEY']
+  Bugsnag.configure do |config|
+    config.endpoint = ENV.fetch('BUGSNAG_ENDPOINT', 'notify.bugsnag.com')
+    config.api_key = ENV['BUGSNAG_API_KEY']
+    config.release_stage = 'production'
+  end
+
+  # Hook at_exit to send off the fatal exception if it occurred
+  at_exit { Bugsnag.notify($ERROR_INFO) if $ERROR_INFO }
+end
+
 module Salus
   module SalusBugsnag
     def bugsnag_notify(msg)
