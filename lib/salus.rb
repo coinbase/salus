0ff6c73df2fd466d2b03d9c61f8fb22c86531e6b
Brianna Ren <brianna.ren@coinbase.com>
2020-07-07 16:59:13 -0700

    version bump to 2.10.4

--- lib/salus.rb
+++ lib/salus.rb
@@ -8,7 +8,7 @@ require 'salus/config'
 require 'salus/processor'
 
 module Salus
-  VERSION = '2.10.3'.freeze
+  VERSION = '2.10.4'.freeze
   DEFAULT_REPO_PATH = './repo'.freeze # This is inside the docker container at /home/repo.
 
   SafeYAML::OPTIONS[:default_mode] = :safe
