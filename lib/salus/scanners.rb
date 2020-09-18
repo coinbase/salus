aee295867145ac87981c595365e11eea5b1b2711
joshua-ostrom <joshua.ostrom@coinbase.com>
2020-09-18 10:52:44 -0400

    rubo nit

--- lib/salus/scanners.rb
+++ lib/salus/scanners.rb
@@ -4,5 +4,6 @@ module Salus::Scanners; end
 Dir.entries(File.expand_path('scanners', __dir__)).sort.each do |filename|
   next if ['.', '..'].include?(filename) # don't include FS pointers
   next unless /\.rb\z/.match?(filename)  # only include ruby files
+
   require "salus/scanners/#{filename}"
 end
