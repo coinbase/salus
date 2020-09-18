d28b1bfc3be3d6895d99ced7146788e698456bbd
joshua-ostrom <joshua.ostrom@coinbase.com>
2020-09-18 15:44:45 -0400

    Attempting to kickstart circle ci

--- lib/salus/scanners.rb
+++ lib/salus/scanners.rb
@@ -1,6 +1,6 @@
 module Salus::Scanners; end
 
-# Sort to avoid race coniditions in what order scanners are loaded
+# Sort to avoid race coniditions in what order scanners are loaded.
 Dir.entries(File.expand_path('scanners', __dir__)).sort.each do |filename|
   next if ['.', '..'].include?(filename) # don't include FS pointers
   next unless /\.rb\z/.match?(filename)  # only include ruby files
