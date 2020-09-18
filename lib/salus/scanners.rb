80f244067964b428691c239606f6daba31c6cac1
joshua-ostrom <joshua.ostrom@coinbase.com>
2020-09-18 09:17:31 -0400

    Ensure a reproducable load order and add missing require

--- lib/salus/scanners.rb
+++ lib/salus/scanners.rb
@@ -1,8 +1,8 @@
 module Salus::Scanners; end
 
-Dir.entries(File.expand_path('scanners', __dir__)).each do |filename|
+# We'll sort to avoid any race conditions in loading order
+Dir.entries(File.expand_path('scanners', __dir__)).sort.each do |filename|
   next if ['.', '..'].include?(filename) # don't include FS pointers
   next unless /\.rb\z/.match?(filename)  # only include ruby files
-
   require "salus/scanners/#{filename}"
 end
