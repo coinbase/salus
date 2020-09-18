2db545e207d628b92109292623787ce61d1d841a
joshua-ostrom <joshua.ostrom@coinbase.com>
2020-09-18 13:39:29 -0400

    Removed race condition and added missing require

--- lib/salus/scanners.rb
+++ lib/salus/scanners.rb
@@ -1,6 +1,7 @@
 module Salus::Scanners; end
 
-Dir.entries(File.expand_path('scanners', __dir__)).each do |filename|
+# Sort to avoid race coniditions in what order scanners are loaded
+Dir.entries(File.expand_path('scanners', __dir__)).sort.each do |filename|
   next if ['.', '..'].include?(filename) # don't include FS pointers
   next unless /\.rb\z/.match?(filename)  # only include ruby files
 
