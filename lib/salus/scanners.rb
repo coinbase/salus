6f5aa2eed3355d6843dd122824bc2f09b9f874e2
joshua-ostrom <joshua.ostrom@coinbase.com>
2020-09-22 09:36:53 -0400

    Verifying gpg

--- lib/salus/scanners.rb
+++ lib/salus/scanners.rb
@@ -1,6 +1,6 @@
 module Salus::Scanners; end
 
-# Sort to avoid race conditions caused by differing load orders
+# Sort to avoid race conditions caused by differing load orders.
 Dir.entries(File.expand_path('scanners', __dir__)).sort.each do |filename|
   next if ['.', '..'].include?(filename) # don't include FS pointers
   next unless /\.rb\z/.match?(filename)  # only include ruby files
