98f83d5396fee4b86184b7728760ba0842912cb8
kamsiy <kamsiyoib@gmail.com>
2021-02-03 10:35:19 -0600

    Update BaseSarif tests

--- spec/lib/sarif/base_sarif_spec.rb
+++ spec/lib/sarif/base_sarif_spec.rb
@@ -4,19 +4,40 @@ require 'json-schema'
 describe Sarif::BaseSarif do
   let(:scan_report) { Salus::ScanReport.new("Bandit") }
   let(:base_sarif) { Sarif::BaseSarif.new(scan_report) }
-
+  let(:report) { Salus::Report.new(project_name: 'Neon genesis') }
   before do
     scan_report.add_version('1.1.1')
   end
 
-  describe 'sarif_report' do
+  describe 'tool_info' do
     it 'returns the runs object for an unsupported scanner' do
-      schema = JSON.parse(File.read('spec/fixtures/sarif/sarif-schema.json'))
-      body = JSON.pretty_generate({ "version" => "2.1.0",
-        "$schema" => "https://schemastore.azurewebsites.net/schemas"\
-        "/json/sarif-2.1.0-rtm.5.json",
-        "runs" => [base_sarif.sarif_report] })
-      expect(JSON::Validator.validate(schema, JSON.parse(body))).to be true
+      expect(base_sarif.tool_info).to include({ "driver":
+        {
+          "name" => "Bandit",
+          "version" => "1.1.1"
+        } })
+    end
+  end
+
+  describe 'conversion' do
+    it 'returns the conversion object for the converter (Salus)' do
+      expect(base_sarif.conversion).to include({ "tool":
+        {
+          "driver": {
+            "name": "Salus",
+            "informationUri": "https://github.com/coinbase/salus"
+          }
+        } })
+    end
+  end
+
+  describe 'sarif_report' do
+    it 'returns' do
+      expect(base_sarif.sarif_report).to include({ "tool" => base_sarif.tool_info,
+        "conversion" => base_sarif.conversion,
+        "results" => [],
+        "invocations" => base_sarif.invocations
+      })
     end
   end
 end
