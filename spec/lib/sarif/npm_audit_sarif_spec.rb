50ee9f54570166b0fb7576e15d7862e7a933eea9
Kamsiy <kamsiyoib@gmail.com>
2021-02-18 13:12:54 -0600

    Update tests

--- spec/lib/sarif/npm_audit_sarif_spec.rb
+++ spec/lib/sarif/npm_audit_sarif_spec.rb
@@ -19,7 +19,7 @@ describe Sarif::NPMAuditSarif do
           details: "Package:uglify-js \nDescription:Versions of `uglify-js` prior to 2.4.24 are"\
           " affected by a vulnerability which may cause crafted JavaScript to have altered"\
           " functionality after minification.\n\n \nRecommendation: Upgrade UglifyJS to version"\
-          " >= 2.4.24.\nVulnerable Versions: <= 2.4.23 \nSeverity:lowPatched Versions: >= 2.4.24"\
+          " >= 2.4.24.\nVulnerable Versions: <= 2.4.23 \nSeverity:low \nPatched Versions: >= 2.4.24"\
           "\nCWE: CWE-95 ",
           help_url: "https://npmjs.com/advisories/39",
           uri: "package-lock.json"
