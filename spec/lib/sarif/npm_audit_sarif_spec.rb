2fe3558b7676d3a7cc62dd94617de37e8b669565
kamsi-ibeziako <kamsi.ibeziako@coinbase.com>
2021-03-10 11:23:43 -0600

    Update tests

--- spec/lib/sarif/npm_audit_sarif_spec.rb
+++ spec/lib/sarif/npm_audit_sarif_spec.rb
@@ -120,7 +120,6 @@ describe Sarif::NPMAuditSarif do
         report = Salus::Report.new(project_name: "Neon Genesis")
         report.add_scan_report(scanner.report, required: false)
         report_object = JSON.parse(report.to_sarif)['runs'][0]
-        expect(report_object['results'].empty?).to eq(true)
         expect(report_object['invocations'][0]['executionSuccessful']).to eq(true)
       end
     end
