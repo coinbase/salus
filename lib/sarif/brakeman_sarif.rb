904a5a5526e5db24c06bfd6bfbdbc305e77c28f0
kamsi-ibeziako <kamsi.ibeziako@coinbase.com>
2021-03-09 23:03:33 -0600

    Remove duplicate error code

--- lib/sarif/brakeman_sarif.rb
+++ lib/sarif/brakeman_sarif.rb
@@ -1,20 +1,25 @@
 module Sarif
   class BrakemanSarif < BaseSarif
+    include Salus::SalusBugsnag
+
     BRAKEMAN_URI = 'https://github.com/presidentbeef/brakeman'.freeze
 
     def initialize(scan_report)
       super(scan_report)
       @uri = BRAKEMAN_URI
-      begin
-        @logs = JSON.parse(scan_report.log(''))['warnings']
-      rescue JSON::ParserError
-        @logs = []
-      end
+      @logs = parse_scan_report!
+    end
+
+    def parse_scan_report!
+      JSON.parse(@scan_report.log(''))['warnings']
+    rescue JSON::ParserError => e
+      bugsnag_notify(e.message)
+      []
     end
 
     def parse_issue(issue)
       {
-        id: format('BRAKE%<number>.4d', number: issue['warning_code'].to_i),
+        id: issue['warning_code'],
         name: "#{issue['check_name']}/#{issue['warning_type']}",
         level: issue['confidence'].upcase,
         details: issue['message'],
@@ -24,25 +29,6 @@ module Sarif
         help_url: issue['link'],
         code: issue['code']
       }
-    end
-
-    def build_invocations
-      if @logs.empty? && !@scan_report.passed?
-        {
-          "executionSuccessful": false,
-          "toolExecutionNotifications": [{
-            "descriptor": {
-              "id": ""
-            },
-            "level": "error",
-            "message": {
-              "text": @scan_report.to_h.fetch(:errors).first[:message]
-            }
-          }]
-        }
-      else
-        { "executionSuccessful": @scan_report.passed? }
-      end
     end
   end
 end
