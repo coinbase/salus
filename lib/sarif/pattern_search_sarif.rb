d60493007ef38495a81618049c036fd21ef07f96
kamsi-ibeziako <kamsi.ibeziako@coinbase.com>
2021-03-16 08:39:08 -0500

    add support for patternsearch

---
+++ lib/sarif/pattern_search_sarif.rb
@@ -0,0 +1,41 @@
+require 'salus/bugsnag'
+
+module Sarif
+  class PatternSearchSarif < BaseSarif
+    include Salus::SalusBugsnag
+
+    PATTERN_SEARCH_URI = "https://github.com/coinbase/salus/blob/master/docs/scanners/"\
+    "pattern_search.md".freeze
+
+    def initialize(scan_report)
+      super(scan_report)
+      @uri = PATTERN_SEARCH_URI
+      @logs = scan_report.to_h.dig(:info, :hits)
+    end
+
+    def parse_issue(issue)
+      url_info = issue[:hit].split(':')
+      {
+        id: issue[:regex],
+        name: "Regex: issue[:regex]",
+        level: issue[:forbidden],
+        details: "Regex: #{issue[:regex]}\nForbidden: #{issue[:forbidden]}\nMessage:#{issue[:msg]}"\
+        "\nRequired: #{issue[:forbidden]}",
+        start_line: url_info[1],
+        start_column: 0,
+        uri: url_info[0],
+        help_url: PATTERN_SEARCH_URI,
+        code: issue[:hit]
+      }
+    end
+
+    def sarif_level(severity)
+      case severity
+      when true
+        SARIF_WARNINGS[:error]
+      else
+        SARIF_WARNINGS[:warning]
+      end
+    end
+  end
+end
