d02791fc1cd6588ce0232be546eed4c63e8638ea
Saron Debalkew <saron.debalkew@coinbase.com>
2021-07-13 14:13:00 -0400

    add tests for salusEnforced bool

--- spec/lib/sarif/base_sarif_spec.rb
+++ spec/lib/sarif/base_sarif_spec.rb
@@ -144,6 +144,48 @@ describe Sarif::BaseSarif do
         expect(runs_object['results'].empty?).to eq(false)
         expect(runs_object["invocations"][0][:executionSuccessful]).to eq(false)
       end
+      binding.pry
+      it 'has salusEnforced false when supported scanner is not enforced' do
+        parsed_issue = {
+          id: 'SAL002',
+          name: "Golang Error",
+          level: "NOTE",
+          details: 'error',
+          start_line: 1,
+          start_column: 1,
+          uri: '',
+          help_url: "https://github.com/coinbase/salus/blob/master/docs/salus_reports.md",
+          code: ""
+        }
+        adapter = Sarif::GosecSarif.new(scan_report)
+        adapter.instance_variable_set(:@logs, [parsed_issue])
+        adapter.instance_variable_set(:@config, { "include_suppressed": true }.stringify_keys)
+        adapter.instance_variable_set(:@required, false)
+        runs_object = adapter.build_runs_object(true)
+        expect(runs_object['tool'][:driver]['properties'][:salusEnforced]).to eq(false)
+        binding.pry
+      end
+  
+      it 'has salusEnforced true when supported scanner is enforced' do
+        parsed_issue = {
+          id: 'SAL002',
+          name: "Golang Error",
+          level: "NOTE",
+          details: 'error',
+          start_line: 1,
+          start_column: 1,
+          uri: '',
+          help_url: "https://github.com/coinbase/salus/blob/master/docs/salus_reports.md",
+          code: ""
+        }
+        adapter = Sarif::GosecSarif.new(scan_report)
+        adapter.instance_variable_set(:@logs, [parsed_issue])
+        adapter.instance_variable_set(:@config, { "include_suppressed": false }.stringify_keys)
+        adapter.instance_variable_set(:@required, true)
+        runs_object = adapter.build_runs_object(true)
+        expect(runs_object['tool'][:driver]['properties'][:salusEnforced]).to eq(true)
+      end
+
       it 'results are not included for non enforced scanners when include_suppressed is false' do
         parsed_issue = {
           id: 'SAL002',
