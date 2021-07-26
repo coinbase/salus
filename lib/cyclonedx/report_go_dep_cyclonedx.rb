bee65317f95eaac5a0faeb0636f87203ac60c436
jeffrey-zhang <jeffrey.zhang@coinbase.com>
2021-07-26 15:27:04 -0400

    add concrete scanner files

---
+++ lib/cyclonedx/report_go_dep_cyclonedx.rb
@@ -0,0 +1,8 @@
+
+module Cyclonedx
+  class ReportGoDepCyclonedx < BaseCyclonedx
+    def initialize(scan_report)
+      super(scan_report)
+    end
+  end
+end
---
+++ lib/cyclonedx/report_python_modules_cyclonedx.rb
@@ -0,0 +1,8 @@
+
+module Cyclonedx
+  class ReportPythonModulesCyclonedx < BaseCyclonedx
+    def initialize(scan_report)
+      super(scan_report)
+    end
+  end
+end
