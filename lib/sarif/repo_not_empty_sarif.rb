36cf4516fa49c5f70141cf27bd9ec4a53d7b5ba3
Gerald Nash <gerald.nash@coinbase.com>
2021-08-18 11:49:29 -0400

    Add a SARIF dapter for RepoNotEmpty

---
+++ lib/sarif/repo_not_empty_sarif.rb
@@ -0,0 +1,31 @@
+require 'salus/bugsnag'
+
+module Sarif
+  class RepoNotEmptySarif < BaseSarif
+    include Salus::SalusBugsnag
+
+    NOT_FOUND = "Required Pattern Not Found".freeze
+    REPO_NOT_EMPTY_URI = "https://github.com/coinbase/salus/blob/master/docs/scanners/"\
+    "repository_not_blank.md".freeze
+
+    def initialize(scan_report)
+      super(scan_report)
+      @uri = REPO_NOT_EMPTY_URI
+      @logs = parse_scan_report!
+    end
+
+    def parse_scan_report!
+      @scan_report.errors
+    end
+
+    def parse_issue(issue)
+      {
+        id: "RNE0001",
+        name: "RepositoryIsEmpty",
+        details: issue[:message],
+        level: "HIGH",
+        help_url: REPO_NOT_EMPTY_URI
+      }
+    end
+  end
+end
---
+++ spec/lib/sarif/repo_not_empty_sarif_spec.rb
@@ -0,0 +1,58 @@
+require_relative '../../spec_helper'
+
+describe Sarif::RepoNotEmptySarif do
+  describe '#build_runs_object' do
+    let(:scanner) { Salus::Scanners::RepoNotEmpty.new(repository: repo, config: {}) }
+    before { scanner.run }
+
+    context 'vulnerabilites found in project' do
+      let(:repo) { Salus::Repo.new('spec/fixtures/repo_not_empty/blank') }
+      it 'creates valid sarif report with results populated' do
+        adapter = Sarif::RepoNotEmptySarif.new(scanner.report)
+        report = adapter.build_runs_object(true)
+        rules = report['tool'][:driver]['rules']
+        results = report['results']
+
+        expect(results.size).to eq(1)
+        expect(results).to include(
+          {
+            "ruleId": "RNE0001",
+            "ruleIndex": 0,
+            "level": "error",
+            "message": {
+              "text": "Salus was run on a blank directory. This may indicate "\
+               "misconfiguration such as not correctly voluming in the repository to be scanned."
+            },
+            "locations": [
+              {
+                "physicalLocation": {
+                  "artifactLocation": {
+                    "uri": nil,
+                    "uriBaseId": "%SRCROOT%"
+                  }
+                }
+              }
+            ]
+          }
+        )
+        doc = "https://github.com/coinbase/salus/blob/master/docs/scanners/repository_not_blank.md"
+        expect(rules[0]).to include(
+          {
+            "id": "RNE0001",
+            "name": "RepositoryIsEmpty",
+            "fullDescription": {
+              "text": "Salus was run on a blank directory. This may indicate "\
+              "misconfiguration such as not correctly voluming in the repository to be scanned."
+            },
+            "messageStrings": {},
+            "helpUri": doc,
+            "help": {
+              "text": "More info: #{doc}",
+              "markdown": "[More info](#{doc})."
+            }
+          }
+        )
+      end
+    end
+  end
+end
