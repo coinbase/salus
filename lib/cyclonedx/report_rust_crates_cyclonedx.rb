7b96053a096a573e204197c869af8520f3f9b20a
Saron Debalkew <saron.debalkew@coinbase.com>
2021-08-23 15:17:29 -0400

    refacor fscanner fields

--- lib/cyclonedx/report_rust_crates_cyclonedx.rb
+++ lib/cyclonedx/report_rust_crates_cyclonedx.rb
@@ -1,16 +1,10 @@
 module Cyclonedx
   class ReportRustCrates < Base
+    TYPE = "cargo".freeze
+    UNLOCKED_DEPENDENCY_FILE = "".freeze
+
     def initialize(scan_report)
       super(scan_report)
-    end
-
-    def package_url(dependency)
-      "pkg:#{dependency[:type]}/#{dependency[:name]}@#{version_string(dependency)}"
-    end
-
-    # Return version string to be used in purl or component
-    def version_string(dependency)
-      dependency[:version_tag].to_s
     end
   end
 end
