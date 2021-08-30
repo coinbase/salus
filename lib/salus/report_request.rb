f257c1f73ebf6bf8cd2b107249dd65e6b2c19963
Gerald Nash <gerald.nash@coinbase.com>
2021-08-30 12:59:06 -0400

    sarif -> salus_sarif

--- lib/salus/report_request.rb
+++ lib/salus/report_request.rb
@@ -12,7 +12,7 @@ module Salus
     }.freeze
 
     FORMAT_SARIF_DIFF = "sarif_diff".freeze
-    FORMAT_SARIF = "sarif".freeze
+    FORMAT_SARIF = "salus_sarif".freeze
     SCANNER_TYPE_SARIF_DIFF = "salus_sarif_diff".freeze
     SCANNER_TYPE_SARIF = "sarif".freeze
     SCANNER_TYPE_SALUS = "salus".freeze
--- spec/lib/salus/report_spec.rb
+++ spec/lib/salus/report_spec.rb
@@ -293,7 +293,7 @@ describe Salus::Report do
             'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
             'Content-Type' => 'application/json',
             'User-Agent' => 'Faraday v1.3.0',
-            'X-Scanner' => 'sarif' }
+            'X-Scanner' => 'salus_sarif' }
         ).to_return(status: 200, body: "", headers: {})
 
         expect(report).to receive(:to_sarif).with(options).and_call_original.twice
