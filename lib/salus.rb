bf6a6c729627a8acc988e21e4cec0d630140dc0c
joshua-ostrom <joshua.ostrom@coinbase.com>
2021-10-11 16:47:18 -0400

    Version bump to 2.13.4

--- lib/salus.rb
+++ lib/salus.rb
@@ -13,7 +13,7 @@ require 'cyclonedx/report'
 require 'salus/report_request'
 
 module Salus
-  VERSION = '2.13.3'.freeze
+  VERSION = '2.13.4'.freeze
   DEFAULT_REPO_PATH = './repo'.freeze # This is inside the docker container at /home/repo.
 
   SafeYAML::OPTIONS[:default_mode] = :safe
