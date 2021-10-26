d6057e64040f6cda373fefb18195be637e556357
Brianna Ren <brianna.ren@coinbase.com>
2021-10-26 00:31:26 -0700

    fix failed cargo audit tests

--- spec/lib/sarif/cargo_audit_sarif_spec.rb
+++ spec/lib/sarif/cargo_audit_sarif_spec.rb
@@ -33,7 +33,7 @@ describe Sarif::CargoAuditSarif do
           messageStrings: { "package": { "text": "libflate" },
                           "title": { "text": "MultiDecoder::read() drops uninitialized memory of"\
                           " arbitrary type on panic in client code" },
-                          "severity": { "text": "" },
+                          "severity": { "text": "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H" },
                           "patched_versions": { "text": "[\">=0.1.25\"]" },
                           "unaffected_versions": { "text": "[\"<0.1.14\"]" } },
           help_url: "https://github.com/sile/libflate/issues/35",
