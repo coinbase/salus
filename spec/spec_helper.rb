a9d0b549994003e3dbd433f99eeb9b9d0a6872cc
Gerald Nash <gerald.nash@coinbase.com>
2021-11-17 14:04:27 -0500

    Version 2.15.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,7 +88,7 @@ RSpec.configure do |config|
   # recommended. For more details, see:
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-  #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
+  #   - http://rspec.info/blog/2.15.05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
   config.disable_monkey_patching!
 
   # This setting enables warnings. It's recommended, but in some cases may
