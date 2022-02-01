e094a611cd3ab6485c7fa66acfd5fdb4c0915c91
Gerald Nash <gerald.nash@coinbase.com>
2022-02-01 15:12:00 -0500

    Add ReportPodfileLock

--- lib/salus/repo.rb
+++ lib/salus/repo.rb
@@ -32,7 +32,9 @@ module Salus
       { handle: :android_app, filename: '.apk', wildcard: true },
       { handle: :ios_app, filename: '.ipa', wildcard: true },
       # Java
-      { handle: :pom_xml, filename: 'pom.xml' }
+      { handle: :pom_xml, filename: 'pom.xml' },
+      # Apple Ecosystem (macOS, iOS, etc)
+      { handle: :podfile_lock, filename: 'Podfile.lock'}
     ].freeze
 
     # Define file checkers.
---
+++ lib/salus/scanners/report_podfile_lock.rb
@@ -0,0 +1,46 @@
+require 'salus/scanners/base'
+
+# Report Cocoapod usage
+
+module Salus::Scanners
+  class ReportPodfileLock < Base
+
+    def run
+      shell_return = run_shell("bin/parse_podfile_lock #{@repository.path_to_repo}/Podfile.lock", chdir: nil)
+
+      if !shell_return.success?
+        report_error(shell_return.stderr)
+        return
+      end
+
+      dependencies = nil
+      begin
+        dependencies = JSON.parse(shell_return.stdout)
+      rescue JSON::ParserError
+        err_msg = "Could not parse JSON returned by bin/parse_podfile_lock's stdout!"
+        report_stderr(err_msg)
+        report_error(err_msg)
+        return
+      end
+
+      dependencies.each do |dependency|
+        pod = dependency['pod']
+        version = dependency['version']
+        report_dependency(
+          'Podfile.lock',
+          type: 'cocoa',
+          name: pod,
+          version: version
+        )
+      end
+    end
+
+    def should_run?
+      @repository.podfile_lock_present?
+    end
+
+    def self.supported_languages
+      ['swift', 'objective-c']
+    end
+  end
+end
