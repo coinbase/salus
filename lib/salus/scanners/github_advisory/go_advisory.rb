3f0a05dc0b724ef8a044ca68abc03e7df7088f08
Maitray Shah <maitray.shah@coinbase.com>
2022-02-06 21:05:39 -0800

    Finding go dependencies

--- lib/salus/scanners/github_advisory/go_advisory.rb
+++ lib/salus/scanners/github_advisory/go_advisory.rb
@@ -14,9 +14,77 @@ module Salus::Scanners::GithubAdvisory
 
     def find_repo_dependencies
       # Find map of dependencies and versions used by the project
-      dependencies = {
-      }
-      dependencies
+      all_dependencies = []
+      chosen_dependencies = {}
+
+      # Determine go version to check if 'go mod graph' can be ran.
+      version_shell = run_shell("go mod edit -json")
+      begin
+        shell_return_json = JSON.parse(version_shell.stdout)
+      rescue JSON::ParserError => e
+        error_msg = "GoGithubAdvisory: Failed to parse results of 'go mod edit -json' " \
+                    "to determine go version."
+        bugsnag_notify(e.message)
+        report_error(error_msg)
+        return
+      end
+
+      # Run go mod graph
+      # Returns lines in the format of -
+      # cloud.google.com/go@v0.74.0 golang.org/x/text@v0.3.4
+      if shell_return_json.key?("Go")
+        if SemVersion.new(shell_return_json["Go"]) >= SemVersion.new("1.16")
+          raw = run_shell("go mod graph", chdir: @repository.path_to_repo).stdout
+          raw.each_line do |line|
+            direct, indirect = line.split(" ")
+            all_dependencies.append({
+                                      "name": direct.split("@")[0].to_s,
+                                "version": direct.split("@")[1].to_s
+                                    })
+            all_dependencies.append({
+                                      "name": indirect.split("@")[0].to_s,
+                                "version": indirect.split("@")[1].to_s
+                                    })
+          end
+        end
+      end
+
+      # If go mod graph fails / go version less than 1.16, default to go.sum
+      if all_dependencies.empty?
+        go_sum_path = "#{@repository.path_to_repo}/go.sum"
+        File.foreach(go_sum_path).each("=\n") do |line|
+          line = line.strip
+          next if line.empty?
+
+          go_sum_regex = %r{(?<namespace>(.*)(?!/go\.mod))/(?<name>[^\s]*)
+          (\s)*(?<version>(.*))(\s)*h1:(?<checksum>(.*))}x
+
+          if (matches = line.match(go_sum_regex))
+            all_dependencies.append(
+              {
+                "name": (matches[:namespace] + "/" + matches[:name]).to_s,
+                "version": (matches[:version]).to_s.gsub(%r{/go.mod}, '').strip
+              }
+            )
+          end
+        end
+      end
+
+      # Pick specific version of dependencies
+      # If multiple versions of dependencies are found then pick the max version to mimic MVS
+      # https://go.dev/ref/mod#go-mod-graph, https://go.dev/ref/mod#minimal-version-selection
+      all_dependencies.each do |deps|
+        # lib = deps["namespace"] + "/" + deps["name"]
+        lib = deps[:name]
+        version = deps[:version].to_s.gsub('v', '').gsub('+incompatible', '')
+        if chosen_dependencies.key?(lib)
+          temp_v = chosen_dependencies[lib]
+          chosen_dependencies[lib] = version if SemVersion.new(version) > SemVersion.new(temp_v)
+        else
+          chosen_dependencies[lib] = version
+        end
+      end
+      chosen_dependencies
     end
 
     def run
@@ -36,24 +104,24 @@ module Salus::Scanners::GithubAdvisory
         dependencies.each do |lib, version|
           package_matches = github_advisories.select { |v| v.dig("package", "name") == lib }
           package_matches.each do |m|
-            version_match = false
+            version_match_found = false
             version_ranges = m.dig("vulnerableVersionRange").split(', ')
             if version_ranges.length == 1
-              version_match = SemDependency.new('', version_ranges[0]).match?('', version)
+              version_match_found = SemDependency.new('', version_ranges[0]).match?('', version)
             elsif version_ranges.length == 2
-              version_match = SemDependency.new('', version_ranges[0]).match?('', version) &&
+              version_match_found = SemDependency.new('', version_ranges[0]).match?('', version) &&
                 SemDependency.new('', version_ranges[1]).match?('', version)
             end
-            if version_match
+            if version_match_found
               results.append({
                                "Package": m.dig("package", "name"),
-                  "Vulnerable Version": m.dig("vulnerableVersionRange"),
-                  "Version Detected (In Project)": version,
+                  "Vulnerable Version": m.dig("vulnerableVersionRange") || "Not Found",
+                  "Version Detected": version,
                   "Patched Versions": m.dig("firstPatchedVersion", "identifier") || "Not Found",
-                  "Summary": m.dig("advisory", "summary"),
-                  "Severity": m.dig("advisory", "severity"),
-                  "ID": m["advisory"]["identifiers"][0]["value"],
-                  "References": m["advisory"]["references"].collect { |p| p["url"].to_s },
+                  "Summary": m.dig("advisory", "summary") || "Not Found",
+                  "Severity": m.dig("advisory", "severity") || "Not Found",
+                  "ID": m["advisory"]["identifiers"][0]["value"] || "Not Found",
+                  "References": m.dig("advisory", "references").collect { |p| p["url"].to_s },
                   "Source": "Github Advisory"
                              })
             end
