78339ff033b9739ad047fc573c01104aa3b8f650
Gerald Nash <gerald.nash@coinbase.com>
2022-02-09 15:25:32 -0500

    Edit some spec fixtures

--- spec/lib/cyclonedx/report_gradle_deps_cyclonedx_spec.rb
+++ spec/lib/cyclonedx/report_gradle_deps_cyclonedx_spec.rb
@@ -12,60 +12,60 @@ describe Cyclonedx::ReportGradleDeps do
       components_object = gradle_cyclonedx.build_components_object
       expect(components_object.size).to eq(61)
       expect(components_object).to include(
-          {
-            :"bom-ref"=>"pkg:gradle/org.apache.kafka/connect-transforms",
-            :type=>"library",
-            :group=>"",
-            :name=>"org.apache.kafka/connect-transforms",
-            :version=>"2.6.2",
-            :purl=>"pkg:gradle/org.apache.kafka/connect-transforms",
-            :properties=>[
-              {
-                :key=>"source",
-                :value=>""
-              }, 
-              {
-                :key=>"dependency_file",
-                :value=>"build.gradle"
-              }
-            ]
-          },
-          {
-            :"bom-ref"=>"pkg:gradle/org.apache.kafka/connect-api",
-            :type=>"library",
-            :group=>"",
-            :name=>"org.apache.kafka/connect-api",
-            :version=>"2.6.2",
-            :purl=>"pkg:gradle/org.apache.kafka/connect-api",
-            :properties=> [
-              {
-                :key=>"source",
-                :value=>""
-              },
-              {
-                :key=>"dependency_file",
-                :value=>"build.gradle"
-              }
-            ]
-          },
-          {
-            :"bom-ref"=>"pkg:gradle/org.apache.kafka/kafka-clients",
-            :type=>"library",
-            :group=>"",
-            :name=>"org.apache.kafka/kafka-clients",
-            :version=>"2.6.2",
-            :purl=>"pkg:gradle/org.apache.kafka/kafka-clients",
-            :properties=>[
-              {
-                :key=>"source",
-                :value=>""
-              },
-              {
-                :key=>"dependency_file",
-                :value=>"build.gradle"
-              }
-            ]
-          }
+        {
+          "bom-ref": "pkg:gradle/org.apache.kafka/connect-transforms",
+          type: "library",
+          group: "",
+          name: "org.apache.kafka/connect-transforms",
+          version: "2.6.2",
+          purl: "pkg:gradle/org.apache.kafka/connect-transforms",
+          properties: [
+            {
+              key: "source",
+              value: ""
+            },
+            {
+              key: "dependency_file",
+              value: "build.gradle"
+            }
+          ]
+        },
+        {
+          "bom-ref": "pkg:gradle/org.apache.kafka/connect-api",
+          type: "library",
+          group: "",
+          name: "org.apache.kafka/connect-api",
+          version: "2.6.2",
+          purl: "pkg:gradle/org.apache.kafka/connect-api",
+          properties: [
+            {
+              key: "source",
+              value: ""
+            },
+            {
+              key: "dependency_file",
+              value: "build.gradle"
+            }
+          ]
+        },
+        {
+          "bom-ref": "pkg:gradle/org.apache.kafka/kafka-clients",
+          type: "library",
+          group: "",
+          name: "org.apache.kafka/kafka-clients",
+          version: "2.6.2",
+          purl: "pkg:gradle/org.apache.kafka/kafka-clients",
+          properties: [
+            {
+              key: "source",
+              value: ""
+            },
+            {
+              key: "dependency_file",
+              value: "build.gradle"
+            }
+          ]
+        }
       )
     end
 
