f59ce49e0de372e3e41c102703a1f3cb16996f17
Gerald Nash <gerald.nash@coinbase.com>
2022-02-14 15:43:35 -0500

    Nits

--- spec/lib/cyclonedx/report_podfile_lock_cyclonedx_spec.rb
+++ spec/lib/cyclonedx/report_podfile_lock_cyclonedx_spec.rb
@@ -8,8 +8,9 @@ describe Cyclonedx::ReportPodfileLock do
       scanner = Salus::Scanners::ReportPodfileLock.new(repository: repo, config: {})
       scanner.run
 
-      maven_cyclonedx = Cyclonedx::ReportPodfileLock.new(scanner.report)
-      expect(maven_cyclonedx.build_components_object).to match_array(
+      cocoa_cyclonedx = Cyclonedx::ReportPodfileLock.new(scanner.report)
+
+      expect(cocoa_cyclonedx.build_components_object).to match_array(
         [
           {
             "bom-ref": "pkg:cocoa/boost-for-react-native@1.63.0",
@@ -53,6 +54,19 @@ describe Cyclonedx::ReportPodfileLock do
 
     it 'should produce valid CycloneDX under normal conditions' do
       repo = Salus::Repo.new('spec/fixtures/report_podfile_lock/normal')
+
+      scanner = Salus::Scanners::ReportPodfileLock.new(repository: repo, config: {})
+      scanner.run
+
+      cyclonedx_report = Cyclonedx::Report.new([[scanner.report, false]],
+                                               { "spec_version" => "1.3" })
+      cyclonedx_report_hash = cyclonedx_report.to_cyclonedx
+
+      expect { Cyclonedx::Report.validate_cyclonedx(cyclonedx_report_hash) }.not_to raise_error
+    end
+
+    it 'should produce valid CycloneDX when an unparseable file is found' do
+      repo = Salus::Repo.new('spec/fixtures/report_podfile_lock/bad_podfile_cant_parse')
 
       scanner = Salus::Scanners::ReportPodfileLock.new(repository: repo, config: {})
       scanner.run
