5aa93fd7b35d6d4a03c6063b23b1b848f24476e4
joshua-ostrom <joshua.ostrom@coinbase.com>
2022-02-24 17:41:27 -0500

    Fixed logic for matcher

--- spec/lib/salus/processor_spec.rb
+++ spec/lib/salus/processor_spec.rb
@@ -8,7 +8,7 @@ RSpec::Matchers.define :match_report_json do |expected|
       json['scans'][scanner].delete(key)
     end
 
-    return json unless json.dig('config', 'report_uris').nil?
+    return json if json.dig('config', 'report_uris').nil?
 
     # Avoid comparing relative and absolute file:///
     json['config']['report_uris'].each_with_index do |endpoint, index|
