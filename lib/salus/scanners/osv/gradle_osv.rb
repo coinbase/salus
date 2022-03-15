75f252c491750eed4292a90668b36180e6f0fd5f
Maitray Shah <maitray.shah@coinbase.com>
2022-03-15 12:56:39 -0700

    Updating gradle shell logic

--- lib/salus/scanners/osv/gradle_osv.rb
+++ lib/salus/scanners/osv/gradle_osv.rb
@@ -106,7 +106,7 @@ module Salus::Scanners::OSV
 
     # Find dependencies from the project
     def find_dependencies
-      shell_return = run_shell('bin/parse_gradle_deps', chdir: nil)
+      shell_return = run_shell(['bin/parse_gradle_deps', @repository.path_to_repo], chdir: nil)
       if !shell_return.success?
         report_error(shell_return.stderr)
         return []
