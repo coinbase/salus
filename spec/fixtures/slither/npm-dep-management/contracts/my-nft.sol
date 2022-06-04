8390ef09be59d7e484186b44fc9f327c910c26c2
Brianna Ren <brianna.ren@coinbase.com>
2022-06-03 17:46:50 -0700

    remove unneeded fixtures

--- spec/fixtures/slither/npm-dep-management/contracts/my-nft.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MNFT") {
-    }
-}
