b4c96fcf802f12f92d3490addd3c891996081fd9
Brianna Ren <brianna.ren@coinbase.com>
2022-06-06 17:08:29 -0700

    remove unneeded code

--- lib/salus/scanners/slither.rb
+++ lib/salus/scanners/slither.rb
@@ -11,8 +11,7 @@ module Salus::Scanners
     def should_run?
       # only support truffle/hardhat for now
       return false if !@repository.package_json_present? || !has_package_config
-
-      @repository.sol_file_present? != false
+      @repository.sol_file_present?
     end
 
     def run
