4680b144b645c88249c97896375a6f866fe0f9b4
Gerald Nash <gerald.nash@coinbase.com>
2022-08-10 11:10:24 -0400

    Remove spec focusing

--- spec/lib/salus/scanners/yarn_audit_spec.rb
+++ spec/lib/salus/scanners/yarn_audit_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../../spec_helper.rb'
 
-describe Salus::Scanners::YarnAudit, :focus do
+describe Salus::Scanners::YarnAudit do
   describe '#should_run?' do
     it 'should return false in the absence of package.json and friends' do
       repo = Salus::Repo.new('spec/fixtures/blank_repository')
