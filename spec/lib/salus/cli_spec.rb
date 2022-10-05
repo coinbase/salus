f035c43d8d1f0ee681afc3bfda66303cae2deb91
Brianna Ren <brianna.ren@coinbase.com>
2022-10-05 13:15:07 -0700

    test ignore

--- spec/lib/salus/cli_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../spec_helper.rb'
-
-describe Salus::CLI do
-  it 'returns non-zero exit when invalid params are passed' do
-    expect { Salus::CLI.start(['foo bar']) }.to raise_error(SystemExit) do |error|
-      expect(error.status).to eq(1)
-    end
-  end
-end
